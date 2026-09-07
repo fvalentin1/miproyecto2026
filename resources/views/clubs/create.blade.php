c18c0ed70e99d9f3cfa5a3f4f6544a14f3c177da
Felipe V <fvalentin@ing.ucsc.cl>
2026-09-07 15:10:12 -0300

    Update views and routes

--- resources/views/clubs/create.blade.php
+++ resources/views/clubs/create.blade.php
@@ -1,68 +1,103 @@
 <x-app-layout>
-<x-slot name="header">
+    <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Clubes de Futbol - Crear') }}
         </h2>
     </x-slot>
 
-<body>
+    <div class="py-12">
 
-    <h2>Ingrese los datos del club a registrar</h2>
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
 
-    @if($errors->any())
-        <div>
-            <h2>Errores</h2>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
+            <h2>Ingrese los datos del club a registrar</h2>
 
-    <form action="{{ route('clubs.store') }}" method="POST">
-        @csrf
-        <div>
-            <label for="name">Nombre:</label>
-            <input type="text" id="name" name="name" value="" placeholder="Nombre Equipo FC">
-        </div>
-        <br>
-        <div>
-            <label for="titles">Títulos:</label>
-            <input type="number" id="titles" name="titles" value="0" min="0">
-        </div>
-        <br>
-        <div>
-            <label for="city">Ciudad:</label>
-            <input type="text" id="city" name="city" value="" placeholder="">
-        </div>
-        <br>
-        <div>
-            <label for="country">Pais:</label>
-            <input type="text" id="country" name="country" value="">
-        </div>
-        <br>
-        <div>
-            <label for="colors">Colores:</label>
-            <input type="text" name="colors" id="colors" value="">
-        </div>
-        <br>
-        <div>
-            <label for="stadium">Estadio:</label>
-            <input type="text" name="stadium" id="stadium" value="">
-        </div>
-        <br>
-        <div>
-            <label for="founded_year">Fundación:</label>
-            <input type="number" name="founded_year" id="founded_year" min="1800" max="2026" value="2010">
+            @if ($errors->any())
+                <div>
+                    <h2>Errores</h2>
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+
+            <form action="{{ route('clubs.store') }}" method="POST">
+                @csrf
+
+
+                <label for="name" class="text-black">
+                    <span class="text-sm font-semibold">Nombre:</span>
+
+                    <input type="text" id="name" name="name" value="" placeholder="Nombre Equipo FC"
+                        class="mt-0.5 mb-4 w-full border-2 border-black bg-white shadow-[4px_4px_0_0] shadow-black focus:ring-2 focus:ring-yellow-300 sm:text-sm" />
+                </label>
+
+                <br>
+
+                <label for="titles" class="text-black">
+                    <span class="text-sm font-semibold"> Títulos </span>
+
+                    <input type="number" id="titles" name="titles" value="0" min="0"
+                        class="mt-0.5 mb-4 w-full border-2 border-black bg-white shadow-[4px_4px_0_0] shadow-black focus:ring-2 focus:ring-yellow-300 sm:text-sm" />
+                </label>
+
+                <br>
+
+                <label for="city" class="text-black">
+                    <span class="text-sm font-semibold"> Ciudad </span>
+
+                    <input type="text" id="city" name="city" value=""
+                        class="mt-0.5 mb-4 w-full border-2 border-black bg-white shadow-[4px_4px_0_0] shadow-black focus:ring-2 focus:ring-yellow-300 sm:text-sm" />
+                </label>
+
+                <br>
+
+                <label for="country" class="text-black">
+                    <span class="text-sm font-semibold"> Pais </span>
+
+                    <input type="text" id="country" name="country" value=""
+                        class="mt-0.5 mb-4 w-full border-2 border-black bg-white shadow-[4px_4px_0_0] shadow-black focus:ring-2 focus:ring-yellow-300 sm:text-sm" />
+                </label>
+
+                <br>
+
+                <label for="colors" class="text-black">
+                    <span class="text-sm font-semibold"> Colores </span>
+
+                    <input type="text" id="colors" name="colors" value=""
+                        class="mt-0.5 mb-4 w-full border-2 border-black bg-white shadow-[4px_4px_0_0] shadow-black focus:ring-2 focus:ring-yellow-300 sm:text-sm" />
+                </label>
+
+                <br>
+
+                <label for="stadium" class="text-black">
+                    <span class="text-sm font-semibold"> Estadio </span>
+
+                    <input type="text" id="stadium" name="stadium" value=""
+                        class="mt-0.5 mb-4 w-full border-2 border-black bg-white shadow-[4px_4px_0_0] shadow-black focus:ring-2 focus:ring-yellow-300 sm:text-sm" />
+                </label>
+
+                <br>
+
+                <label for="founded_year" class="text-black">
+                    <span class="text-sm font-semibold"> Fundación </span>
+
+                    <input type="number" id="founded_year" name="founded_year" value="2010" min="1800" max="2026"
+                        class="mt-0.5 mb-4 w-full border-2 border-black bg-white shadow-[4px_4px_0_0] shadow-black focus:ring-2 focus:ring-yellow-300 sm:text-sm" />
+                </label>
+
+                <br>
+                <x-button variant="primary" type="submit" class="mt-4">
+                    Crear Club
+                </x-button>
+                {{-- <button type="submit">Crear club</button> --}}
+            </form>
+
+
+
         </div>
 
-        <br>
-        <x-button variant="primary" type="submit">
-            Crear Club
-        </x-button>
-        {{-- <button type="submit">Crear club</button> --}}
-    </form>
+    </div>
 
-</body>
 </x-app-layout>
--- resources/views/clubs/index.blade.php
+++ resources/views/clubs/index.blade.php
@@ -5,89 +5,112 @@
         </h2>
     </x-slot>
 
+    <div class="py-12">
 
-    <p>
-        <a href="{{ route('clubs.create') }}">
-            <x-button variant="success">Crear un club</x-button>
-        </a>
-    </p>
-
-    <hr>
-
-    @if(session('success'))
-        <p>
-            {{ session('success') }}
-        </p>
-    @endif
-
-    @if($clubs->count()>0)
-
-        <table border="1">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Nombre</th>
-                    <th>Titulos</th>
-                    <th>Ciudad</th>
-                    <th>Pais</th>
-                    <th>Colores</th>
-                    <th>Estadio</th>
-                    <th>Fundación</th>
-                    <th>Acciones</th>
-                </tr>
-            </thead>
-
-            <tbody>
-                @foreach ($clubs as $club)
-                <tr>
-                    <td>{{ $club->id }}</td>
-                    <td>{{ $club->name }}</td>
-                    <td>{{ $club->titles }}</td>
-                    <td>{{ $club->city }}</td>
-                    <td>{{ $club->country }}</td>
-                    <td>{{ $club->colors }}</td>
-                    <td>{{ $club->stadium }}</td>
-                    <td>{{ $club->founded_year }}</td>
-
-                    <td>
-                        <a href="{{ route('clubs.show', $club) }}">
-                            <x-button variant="info">Ver Club</x-button>
-                        </a>
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
 
 
-                        <a href="{{ route('clubs.edit', $club) }}">
-                            <x-button variant="warning">Editar Club</x-button>
-                        </a>
+            <p class="mb-4">
+                <a href="{{ route('clubs.create') }}">
+                    <x-button variant="success">Crear un club</x-button>
+                </a>
+            </p>
 
-                        <form action="{{ route('clubs.destroy', $club) }}"
-                            method="POST"
-                            style="display:inline;"
-                        >
+            <hr>
 
-                        @csrf
-                        @method('DELETE')
+            @if (session('success'))
+                <p>
+                    {{ session('success') }}
+                </p>
+            @endif
 
-                        <x-button variant="danger" type="submit">Borrar Club</x-button>
+            @if ($clubs->count() > 0)
 
-                        {{-- <button type="submit">Borrar Club</button> --}}
+                <table border="1">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Nombre</th>
+                            <th>Titulos</th>
+                            <th>Ciudad</th>
+                            <th>Pais</th>
+                            <th>Colores</th>
+                            <th>Estadio</th>
+                            <th>Fundación</th>
+                            <th>Acciones</th>
+                        </tr>
+                    </thead>
 
-                        </form>
+                    <tbody>
+                        @foreach ($clubs as $club)
+                            <tr>
+                                <td>{{ $club->id }}</td>
+                                <td>{{ $club->name }}</td>
+                                <td>{{ $club->titles }}</td>
+                                <td>{{ $club->city }}</td>
+                                <td>{{ $club->country }}</td>
+                                <td>{{ $club->colors }}</td>
+                                <td>{{ $club->stadium }}</td>
+                                <td>{{ $club->founded_year }}</td>
 
-                    </td>
+                                <td>
+                                    <a href="{{ route('clubs.show', $club) }}" title="Ver club">
+                                        <x-button variant="info" aria-label="Ver club">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                stroke-width="1.5" stroke="currentColor" class="size-6">
+                                                <path stroke-linecap="round" stroke-linejoin="round"
+                                                    d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                                                <path stroke-linecap="round" stroke-linejoin="round"
+                                                    d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                            </svg>
 
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-
-    @else
-
-    <p>No hay clubes registrados</p>
-
-    @endif
+                                        </x-button>
+                                    </a>
 
 
+                                    <a href="{{ route('clubs.edit', $club) }}" title="Editar club">
+                                        <x-button variant="warning" aria-label="Editar club">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                stroke-width="1.5" stroke="currentColor" class="size-6">
+                                                <path stroke-linecap="round" stroke-linejoin="round"
+                                                    d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                                            </svg>
+                                        </x-button>
+                                    </a>
 
-</body>
+                                    <form action="{{ route('clubs.destroy', $club) }}" method="POST"
+                                        style="display:inline;">
+
+                                        @csrf
+                                        @method('DELETE')
+
+                                        <x-button variant="danger" type="submit">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                stroke-width="1.5" stroke="currentColor" class="size-6">
+                                                <path stroke-linecap="round" stroke-linejoin="round"
+                                                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                            </svg>
+
+                                        </x-button>
+
+                                        {{-- <button type="submit">Borrar Club</button> --}}
+
+                                    </form>
+
+                                </td>
+
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            @else
+                <p>No hay clubes registrados</p>
+
+            @endif
+        </div>
+
+    </div>
+
+
 
 </x-app-layout>
